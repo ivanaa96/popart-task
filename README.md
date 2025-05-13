6b024aae9a2afce1ef33b7a331f5c7f2827ee1bd
ivanaa96 <ivana.stanic1@hotmail.com>
2025-05-13 11:31:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# PopArt Studio – Frontend Task (Ivana Stanic)
+
+This is a frontend project built with HTML5, SCSS (Sass), JavaScript, and Gulp. The design is based on the provided Figma file.
+
+## Requirements
+
+- Node.js and npm installed
+- Gulp CLI (optional but recommended)
+
+## Getting Started
+
+### 1. Clone the repository:
+
+git clone <repo-url>
+cd popart-task
+
+### 2. Install dependencies:
+
+- npm ci
+
+### 3. Start the development server:
+
+npx gulp
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+gulp.task("sass", () => {
+  return gulp
+    .src("./src/scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("./src/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task("serve", () => {
+  browserSync.init({
+    server: "./src",
+  });
+
+  gulp.watch("./src/scss/**/*.scss", gulp.series("sass"));
+  gulp.watch("./src/*.html").on("change", browserSync.reload);
+  gulp.watch("./src/js/**/*.js").on("change", browserSync.reload);
+});
+
+gulp.task("default", gulp.series("sass", "serve"));
